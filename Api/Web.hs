0be0b16fdd07ab4505075696b916a66a6781c713
Révész Ádám <reveszadam@gmail.com>
2017-09-03 09:43:36 +0200

    webAPI and some cleanup

---
+++ Api/Web.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators     #-}
+
+module Api.Web(Api.Web.run) where
+
+import           Control.Lens
+import           Control.Monad.Trans.Except
+import           Data.Aeson
+import           Data.Aeson.Encode.Pretty   (encodePretty)
+import           Data.Proxy
+import           Data.Text                  (Text)
+import           Data.Text.Encoding
+import           Data.Typeable              (Typeable)
+import           GHC.Generics
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+import           Persistence.Handle
+import           Servant
+import           System.IO                  (hPutStrLn, stderr)
+
+nameDayApi :: Proxy NameDayApi
+nameDayApi = Proxy
+
+type NameDayApi = Capture "month" Int :> Capture "day" Int :> Get '[JSON] [Text]
+
+serveGetNamesFor :: Handle -> Int -> Int -> Handler [Text]
+serveGetNamesFor h m d = return $ (namesFor h (m, d))
+
+server :: Handle -> Server NameDayApi
+server h =  (serveGetNamesFor h)
+
+mkApp :: Handle -> Application
+mkApp h = serve nameDayApi $ server h
+
+run :: Int -> Handle -> IO ()
+run p handle = do
+    Network.Wai.Handler.Warp.run p $ mkApp handle
--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import           Api.Web            (run)
 import           Persistence.FileDB
 import           Persistence.Handle
 import           System.Environment (getArgs)
@@ -7,6 +8,7 @@ import           System.Environment (getArgs)
 main :: IO ()
 main = do
     args <- getArgs
-    handle <- makeHandle $ head args
-    print $ namesFor handle (read $ args !! 1, read $ args !! 2)
-    -- print "hello"
+    print args
+    -- handle <- makeHandle $ head args
+    -- run (read $ args !! 1) handle
+    -- print $ namesFor handle (read $ args !! 1, read $ args !! 2)
